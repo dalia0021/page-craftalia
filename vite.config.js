30160052c661e5c3bdfc5cd5f52ba4b40632b36c
unknown <dalia@saitenlinea.com>
2023-07-18 12:05:37 -0700

    se cambio base de vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://dalia0021.github.io/page-luharud'
+  base: 'https://craft.luharud.com'
 })
