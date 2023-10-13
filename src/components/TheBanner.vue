5730670e6078480c8c7d22364793a8ec58d220da
unknown <dalia@saitenlinea.com>
2023-10-12 17:28:39 -0700

    se cambio col-12 a col-10 de titulo

--- src/components/TheBanner.vue
+++ src/components/TheBanner.vue
@@ -7,7 +7,7 @@
                 <div class="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-10"> 
                     <img src="../assets/banner-cerezo.png" alt="banner-cerezo" class="img-fluid mx-auto d-block" id="img-banner">
                 </div>
-                <div class="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-12">
+                <div class="col-xl-3 col-lg-4 col-md-5 col-sm-6 col-10">
                     <div class="text-center">
                         <h1 class="text-cafe">Tus Ideas, Nuestra Creatividad</h1>
                         <h6 class="text-lila"><i> Accesorios y Papelería Personalizadas</i></h6>
