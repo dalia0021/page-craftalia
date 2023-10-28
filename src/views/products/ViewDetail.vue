924a61c927dd8e149c966633fa08659c30fee80f
unknown <dalia@saitenlinea.com>
2023-10-28 13:21:47 -0700

    se agrego seccion prodcuto detalle con informacion

--- src/views/products/ViewDetail.vue
+++ src/views/products/ViewDetail.vue
@@ -1,9 +1,10 @@
 <script setup>
 import TheNavbar from "@components/TheNavbar.vue";
 import BaseProductDetalle from "@components/BaseProductDetalle.vue";
+import BaseProduct from "@components/BaseProduct.vue";
 import TheFooter from "@components/TheFooter.vue";
 import { useApi } from "@composables/useApi";
-import { reactive, ref } from "vue";
+import { reactive, ref, computed } from "vue";
 
 const appApi = reactive(useApi());
 
@@ -14,6 +15,7 @@ const props = defineProps({
     },
 });
 
+const listProductos = ref([])
 const producto = reactive({
     image: "",
     category: "",
@@ -29,26 +31,41 @@ const loadData = () => {
     producto.image = data.image;
     producto.category = data.category;
     producto.name = data.name;
+    producto.description = data.description;
     producto.size = data.size;
     producto.price = data.price;
     producto.featured = data.featured;
     producto.thumbnails = data.thumbnails;
+    producto.characteristics = data.characteristics;
 };
+
+const fetchListProducts = () => {
+    listProductos.value =  appApi.getProductsList()
+}
+
+const featuredProducts = computed(() => {
+  return listProductos.value.filter((item) => item.featured == true)
+})
+
+
+fetchListProducts();
 
 loadData();
 </script>
 
 <template>
     <TheNavbar />
-    <div class="container py-5" style="margin-top: 122px">
+    <div class="container p-5" style="margin-top: 122px">
         <BaseProductDetalle
             :image="producto.image"
             :category="producto.category"
             :name="producto.name"
+            :description="producto.description"
             :size="producto.size"
             :price="producto.price"
             :featured="producto.featured"
             :thumbnails="producto.thumbnails"
+            :characteristics="producto.characteristics"
         />
     </div>
     <TheFooter />
