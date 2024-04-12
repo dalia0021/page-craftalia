07ba7724d1fcdca99ba9bfe9750ee0ea57f35e49
dalia0021 <dalia-0021@hotmail.com>
2024-04-12 12:58:53 -0700

    Update vite.config.js - base craftalia.mx

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [vue()],
-    base: "https://craft.luharud.mx",
+    base: "https://craftalia.mx",
     resolve: {
         alias: [
           { find: '@src', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
